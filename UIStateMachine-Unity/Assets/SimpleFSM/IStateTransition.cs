4a2b828842b09b61a063625644f27019ffdf5c69
Go <go05sato@gmail.com>
2018-06-28 20:52:02 +0900

    FSM用のスクリプト追加

---
+++ UIStateMachine-Unity/Assets/SimpleFSM/IStateTransition.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SimpleFSM
+{
+    public interface IStateTransition
+    {
+        IEnumerable Exit();
+        IEnumerable Enter();
+    }
+}
---
+++ UIStateMachine-Unity/Assets/SimpleFSM/StateBeginExitEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace SimpleFSM
+{
+    public class StateBeginExitEventArgs : EventArgs
+    {
+        public IState NextState { get; private set; }
+        public IStateTransition Transition { get; private set; }
+
+        public StateBeginExitEventArgs(IState nextState, IStateTransition transition)
+        {
+            NextState = nextState;
+            Transition = transition;
+        }
+    }
+}
