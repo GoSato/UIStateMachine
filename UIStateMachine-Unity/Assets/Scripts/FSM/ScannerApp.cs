b14f466115f63654f9aa368f75303166d73eb512
Go <go05sato@gmail.com>
2018-06-28 21:42:01 +0900

    ページ切り替え実装

---
+++ UIStateMachine-Unity/Assets/Scripts/FSM/ScannerApp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleFSM;
+using Utility;
+using UnityEngine.UI;
+
+/// <summary>
+/// 初期化用
+/// </summary>
+namespace STYLY.ScannerApp
+{
+    public class ScannerApp : SingletonMonoBehaviour<ScannerApp>
+    {
+        [SerializeField]
+        private GameObject _loginPage;
+        public GameObject LoginPage { get { return _loginPage; } }
+
+        [SerializeField]
+        private GameObject _privacyPage;
+        public GameObject PrivacyPage { get { return _privacyPage; } }
+
+        [SerializeField]
+        private Button _backButton;
+        public Button BackButton { get { return _backButton; } }
+
+        private void Start()
+        {
+            var mainMenuState = new LoginPage();
+            var stateMachine = new StateMachine(mainMenuState);
+            StartCoroutine(stateMachine.Execute().GetEnumerator());
+        }
+    }
+}
